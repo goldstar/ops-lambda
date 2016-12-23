a8b19ba82efdfb26dec1be4b571c56204de500ba
Joe Stump <jstump@goldstar.com>
2016-12-22 16:05:44 -0800

    Don't catch exceptions here. Let them bubble up to CloudWatch Logs.

--- events/handler.js
+++ events/handler.js
@@ -4,12 +4,8 @@ import Processor from './processor';
 
 export function processEvent(event, context, callback) {
   const processor = new Processor(event);
-  try {
-    processor.process((success, errors) => {
-      const msg = "Success = " + success.length + ", Errors = " + errors.length;
-      callback(null, msg);
-    });
-  } catch (e) {
-    callback(null, "ERROR: " + e);
-  }
+  processor.process((success, errors) => {
+    const msg = "Success = " + success.length + ", Errors = " + errors.length;
+    callback(null, msg);
+  });
 };
